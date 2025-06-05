2a4fe8086c10b9d208eaa4599d9cc01ecc42e367
vamsii777 <vamsi.madduluri@icloud.com>
2025-06-06 03:35:45 +0530

    Add Recaptcha v3 support to RecaptchaVersion

--- IdentityToolkit/Sources/Users/Models/Accounts API/Types/RecaptchaVersion.swift
+++ IdentityToolkit/Sources/Users/Models/Accounts API/Types/RecaptchaVersion.swift
@@ -3,6 +3,7 @@ import Foundation
 
 public enum RecaptchaVersion: String, GoogleCloudModel {
     case v2 = "RECAPTCHA_V2"
+    case v3 = "RECAPTCHA_V3"
     case unspecified = "RECAPTCHA_UNSPECIFIED"
     case enterprise = "RECAPTCHA_ENTERPRISE"
 } 
